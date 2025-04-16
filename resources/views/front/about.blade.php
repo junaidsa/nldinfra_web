c7be9f655ba1f3ee7cda0b05ef63f51432b709d0
junaidsa <94611843+junaidsa@users.noreply.github.com>
2025-04-16 13:54:53 +0500

    fkx

--- resources/views/front/about.blade.php
+++ resources/views/front/about.blade.php
@@ -18,7 +18,7 @@
                         <img src="{{ asset('public') }}/front-assets/img/all-images/property/property-img5.png" alt="">
                     </div>
                     <div class="elements reveal image-anime">
-                        <img src="{{ asset('public') }}/front-assets/img/elements/elements9.png" alt="">
+                        {{-- <img src="{{ asset('public') }}/front-assets/img/elements/elements9.png" alt=""> --}}
                     </div>
                 </div>
             </div>
@@ -27,13 +27,27 @@
                 <div class="property-content heading3">
                     <h5 data-aos="fade-left" data-aos-duration="800">{{ $a->about_title }}</h5>
                     <div class="space20"></div>
-
-                    {!! $a->about !!}
-                    <div class="btn-area1" data-aos="fade-left" data-aos-duration="1200">
-                        <a href="{{url('/')}}" class="header-btn4">View Our Property</a>
-                    </div>
+                    <h6>Building More Than Properties — We Build Trust, Comfort & Communities.</h6>
+                    <p>At NLD Developers, we believe that a home is more than just four walls — it’s where life happens. That's why we’re committed to crafting spaces that are thoughtfully designed, expertly built, and perfectly located. Whether you're buying your first home, investing in property, or searching for your next business space, we make the journey smooth, transparent, and rewarding.
+                    </p>
                 </div>
             </div>
+        </div>
+        <div class="space40"></div>
+        <div class="mt-3">
+            <h5 class="text-center mb-3">Who We Are</h5>
+            <p class="text-center">NLD Developers is a forward-thinking real estate development company focused on delivering high-quality residential and commercial projects. With a strong foundation in construction expertise and a passion for innovation, we've earned the trust of clients through honesty, reliability, and consistent excellence.
+            </p>
+            <div class="space20"></div>
+            <h5 class="text-center my-3">🏡 What We Do</h5>
+            <p class="text-center">Residential & Commercial Projects: From cozy apartments to modern office spaces, we develop properties that meet diverse need </p>
+            <p class="text-center">Smart Location Planning: We prioritize convenience and connectivity — our developments are always near essential services, transit, and vibrant neighborhoods.
+            </p>
+            <div class="space20"></div>
+            <p class="text-center">Customer-Centric Approach: From inquiry to handover, we stay by your side, offering complete support, transparency, and peace of mind.</p>
+            <h5 class="text-center my-3">Our Vision</h5>
+            <p class="text-center">To redefine urban living by developing sustainable, community-driven spaces where people can thrive.
+            </p>
         </div>
     </div>
 </div>
